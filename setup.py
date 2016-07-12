b17b0912bddcf79b906c97bb44d1439fdb553911
arttii <a.topchyan@reply.de>
2016-07-12 11:45:38 +0200

    oh god

--- setup.py
+++ setup.py
@@ -10,21 +10,15 @@ install_requires = []
 
 version = '.'.join([str(eventlib.__version__[i]) for i in range(3)])
 
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = 'eventlib',
     version = version,
     packages = ['eventlib',
                 'eventlib.schemaregistry'],
-    install_requires = reqs,
+    install_requires =
+    ['pykafka== 2.3.1', 'fastavro==0.9.9'," avro == 1.8.0","avro-python3 == 1.8.0"]
+   ,
 
     # metadata for upload to PyPI
     author = 'Data Reply',
