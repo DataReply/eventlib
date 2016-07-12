04038e3b85814909229f09fd7edb055abfb2dfc3
arttii <a.topchyan@reply.de>
2016-07-12 11:41:47 +0200

    Fixed versions

--- setup.py
+++ setup.py
@@ -10,16 +10,21 @@ install_requires = []
 
 version = '.'.join([str(eventlib.__version__[i]) for i in range(3)])
 
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt", session=False)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name = 'eventlib',
     version = version,
     packages = ['eventlib',
                 'eventlib.schemaregistry'],
-    install_requires = [
-        "pykafka",
-        "avro-python3"
-
-    ],
+    install_requires = reqs,
 
     # metadata for upload to PyPI
     author = 'Data Reply',
